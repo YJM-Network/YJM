24e6f499ab29bdea6e08b7e18e97bfdce728006d
YJM-Network <kampfflugzeuge@gmail.com>
2025-03-29 00:13:36 +0100

    Update and rename vm.sh to piholes.sh

---
+++ piholes.sh
@@ -0,0 +1,34 @@
+
+#!/bin/bash
+
+
+
+
+
+
+# Verzeichnis für Pi-hole erstellen
+sudo mkdir -p ~/pihole && cd ~/pihole
+
+# Docker-Compose Datei erstellen
+echo "services:
+  pihole:
+    container_name: pihole
+    image: pihole/pihole:latest
+    ports:
+      - \"53:53/tcp\"
+      - \"53:53/udp\"
+      - \"87:80/tcp\"
+      - \"87:443/tcp\"
+    environment:
+      TZ: 'Europe/London'
+      FTLCONF_webserver_api_password: 'correct horse battery staple'
+    volumes:
+      - './etc-pihole:/etc/pihole'
+    cap_add:
+      - NET_ADMIN
+    restart: unless-stopped" > docker-compose.yml
+
+# Pi-hole Container starten
+sudo docker-compose up -d
+
+echo "Installation abgeschlossen."
